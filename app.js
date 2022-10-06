8e14b1a835862301dacead5f7f69c80d084fa473
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-06 17:56:24 +0200

    feat : add a user and show it in the console with asynch await promess

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const User = require('./Users')
 
-mongoose.connect('mongodb://localhost/testdb', ()=>{
-    console.log('connected')
-})
+mongoose.connect('mongodb://localhost/testdb')
+
+const run = async()=>{
+    const user = new User({name : 'md', age: 15})
+    await user.save()
+    console.log(user);
+}
+
+run()
