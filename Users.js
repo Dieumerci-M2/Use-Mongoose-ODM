eea9bdaa8284d631f92db34eec9f06d1a4d28ac4
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-07 11:25:36 +0200

    feat : finish with create method and fix all situations errors which a-can be detected

--- Users.js
+++ Users.js
@@ -1,19 +1,38 @@
 const mongoose = require('mongoose')
 
+const adresse = new mongoose.Schema({
+    quater: String,
+    avenue: String,
+    city: String
+})
+
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number,
-    email : String,
-    createAt : Date,
-    updateAt: Date,
+    age: {
+        type: Number,
+        min: 1,
+        max: 100
+    },
+    email : {
+        type : String,
+        required : true,
+        lowercase : true,
+        minLength : 10
+    },
+    createAt : {
+        type : Date,
+        immutable : true,
+        default : ()=> Date.now()
+    },
+    updateAt:{
+        type: Date,
+        default : ()=> Date.now()
+    },
     // bestFriend: mongoose.SchemaType.ObjectId,
     hobbies: [String],
-    adresse: {
-        quater : String,
-        avenue : String,
-        city: String
-    }
+    adresse: adresse, 
 })
+
 
 const userModel = mongoose.model('User', userSchema)
 
--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const User = require('./Users')
 mongoose.connect('mongodb://localhost/testdb')
 
 const run = async()=>{
-    const user = await User.create({
-        name : 'md',
-        age: 15,
-        hobbies : ['films', 'sport', 'swiming'],
-        adresse: {quater : 'kyeshero', avenue : 'Lusake', city : 'Goma'}
-    })
-    await user.save()
-    console.log(user);
+    try {
+        const user = await User.create({
+            name : 'md',
+            age: 3,
+            hobbies : ['films', 'sport', 'swiming'],
+            adresse: {quater : 'kyeshero', avenue : 'Lusake', city : 'Goma'},
+            email: 'dieumerci.md09@gmail.com'
+        })
+        console.log(user); 
+    } catch (e) {
+       console.log(e.message) 
+    }
 }
 
 run()
