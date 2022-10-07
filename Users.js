31ca686121a0537d7dc744cff774f024fa8ed6ec
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2022-10-07 09:42:18 +0200

    feat : add create method to create users on mongodb DB

--- Users.js
+++ Users.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name: String,
     age: Number,
+    email : String,
+    createAt : Date,
+    updateAt: Date,
+    // bestFriend: mongoose.SchemaType.ObjectId,
+    hobbies: [String],
+    adresse: {
+        quater : String,
+        avenue : String,
+        city: String
+    }
 })
 
-module.exports  = mongoose.model('User', userSchema)
+const userModel = mongoose.model('User', userSchema)
+
+module.exports  = userModel;
--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const User = require('./Users')
 mongoose.connect('mongodb://localhost/testdb')
 
 const run = async()=>{
-    const user = new User({name : 'md', age: 15})
+    const user = await User.create({
+        name : 'md',
+        age: 15,
+        hobbies : ['films', 'sport', 'swiming'],
+        adresse: {quater : 'kyeshero', avenue : 'Lusake', city : 'Goma'}
+    })
     await user.save()
     console.log(user);
 }
